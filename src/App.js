36ba24b0a5a74ae74a5e5ddccc8cd6908357cd9d
brightclass <96156095+brightclass@users.noreply.github.com>
2021-12-24 19:57:37 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -79,7 +79,6 @@ class App extends Component {
   <div class="wrapper">
     <div class="content">
       <div class="item">
-        <!-- Place your content here to have it be centered vertically and horizontally  -->
         <h1>COMING SOON</h1>
         <p>This website is under construction.</p>
       </div>
